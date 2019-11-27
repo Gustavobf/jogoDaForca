1596ce94f91742d7eaab6290b2a778b9ad995c8e
Gustavo Batista <gustavobatistafig@hotmail.com>
2019-11-27 18:55:48 -0300

    Adicionando cabeçalho

--- jogoDaForca.cpp
+++ jogoDaForca.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <map>
+
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe (char chute) {
 	for(char letra : PALAVRA_SECRETA){
@@ -14,25 +17,37 @@ bool letra_existe (char chute) {
 }
 
 
-
 int main() {
 
+	cout << "*********************" << endl;
+	cout << "*** Jogo da Forca ***" << endl;
+	cout << "*********************" << endl;
 
 	bool nao_acertou = true;
 	bool nao_enforcou = true;
 
 	while (nao_acertou && nao_enforcou) {
+
+		for (char letra : PALAVRA_SECRETA) {
+			if (chutou[letra]) {
+				cout << letra << " ";
+			}
+			else {
+				cout << "_ ";
+			}
+		}
+		cout << endl;
+
 		char chute;
 		cin >> chute;
 
+		chutou[chute] = true;
+
 		if (letra_existe(chute)) {
 			cout << "Voce acertou! Seu chute esta na palavra." << endl;
-
 		}
 		else {
 			cout << "Voce errou! Seu chute nao esta na palavra." << endl;
-
-
 		}
 	}
 }
